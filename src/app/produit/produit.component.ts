480405e481bbc9f74a02e9531660e08fa9038769
yapsbjj <ya.ramdane@laposte.net>
2024-02-19 15:41:04 +0100

    add product file

--- src/app/produit/produit.component.ts
+++ src/app/produit/produit.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-produit',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './produit.component.css'
 })
 export class ProduitComponent {
+  products: any[] = [];
+  sortedProducts: any[] = [];
+  sortBy: string = 'price';
 
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this.productService.getProducts()
+      .subscribe(products => {
+        this.products = products;
+        this.sortedProducts = [...this.products];
+      });
+  }
 }
